50e5dd85b09aef6915b43e629c3a0e01c5334146
Towhid0845 <towhid0845@gmail.com>
2024-09-27 10:36:09 +0600

    accordion adder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App_profile_card";
 // import App from "./App_date_count1";
-import App from "./App_date_count2";
+// import App from "./App_date_count2";
+import App from "./App_accordian";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
